2e07ae2a94ade6146548a91f002930e133e05c47
KS1745 <kartikay.sinha17@gmail.com>
2023-09-22 23:58:09 +0530

    first commit

---
+++ Fundamentals-1/script.js
@@ -0,0 +1,90 @@
+/*
+let js = 'amazing';
+console.log(40+8-23-10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let 3years = 3;
+// let jonas&matilda = "JN";
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun );
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 30;
+age = 31;  //mutate the variable
+
+const birthYear = 1991; // immutable variable we can't reassign  const variable
+//birthYear = 1990;
+
+// const job;  // we can't declare empty const variable
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+*/
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 100
+x++;    // x = x + 1
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah);
+
+
